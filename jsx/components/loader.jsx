acab99c495e1cd3f9a4d20bfcb729fa43ade95da
iSkLz <isklzcode@gmail.com>
2020-10-23 16:05:55 +0100

    Added loader component

---
+++ jsx/components/loader.jsx
@@ -0,0 +1,31 @@
+export function Spinner(props) {
+    return (
+        <div className={`spinner-layer spinner-${props.color}`}>
+            <div className="circle-clipper left">
+                <div className="circle"></div>
+            </div>
+            <div className="gap-patch">
+                <div className="circle"></div>
+            </div>
+            <div className="circle-clipper right">
+                <div className="circle"></div>
+            </div>
+        </div>
+    );
+}
+
+export default function Loader(props) {
+    // Believe me, the wrappers are necessary
+    return (
+        <div className="preloader-wrapper-wrapper-wrapper">
+            <div className="preloader-wrapper-wrapper">
+                <div className="preloader-wrapper big active">
+                    <Spinner color="blue" />
+                    <Spinner color="red" />
+                    <Spinner color="yellow" />
+                    <Spinner color="green" />
+                </div>
+            </div>
+        </div>
+    );
+}
