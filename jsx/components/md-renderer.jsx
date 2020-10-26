30e432fb33292354a1b611bc717c49c652fb6f64
iSkLz <isklzcode@gmail.com>
2020-10-26 12:03:31 +0100

    Added DOMPurify

--- jsx/components/md-renderer.jsx
+++ jsx/components/md-renderer.jsx
@@ -1,6 +1,7 @@
 export default function MarkdownRenderer(props) {
 	let ref = (elem) => {
-		elem.innerHTML = marked(props.children);
+		// Sanitize HTML so that it doesn't catch COVID-19
+		$(elem).html(DOMPurify.sanitize(marked(props.children)));
 	};
     return <span ref={ref}></span>
 }
