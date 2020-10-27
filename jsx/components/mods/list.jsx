6d3e9cf2f8d01aed396f3003ff26111aef7ef245
iSkLz <isklzcode@gmail.com>
2020-10-27 18:33:06 +0100

    Implemented sorting options

--- jsx/components/mods/list.jsx
+++ jsx/components/mods/list.jsx
@@ -4,6 +4,7 @@ export default class ModsList extends React.PureComponent {
 
         this.state = {
             search: "",
+            sortBy: "none",
             fuzzySearch: false,
             reverseSort: false
         };
@@ -40,43 +41,76 @@ export default class ModsList extends React.PureComponent {
         }
 
         this.handleOptionChange = (e) => {
-            let state = e.target.getAttribute("state").toString();
-            let value = e.target.type == "checkbox" ? e.target.checked : e.target.value;
+            let state = e.target.type == "radio"
+                ? e.target.name
+                : e.target.getAttribute("state").toString();
+            let value = e.target.type == "checkbox"
+                ? e.target.checked
+                : e.target.value;
 
             // [state] is an ES6 computed property index, in case you don't know it
             this.setState({
                 [state]: value
             });
+        };
+        
+        this.reverseOrderIfNeeded = (order) => {
+            return this.state.reverseSort ? (order == 1 ? -1 : 1) : order;
         }
+
+        this.reverseSorterIfNeeded = (sorter) => {
+            return (itemA, itemB) => {
+                return this.reverseOrderIfNeeded(sorter(itemA, itemB));
+            }
+        };
+
+        this.sortByName = (itemA, itemB) => {
+            return itemA.name.toLowerCase() > itemB.name.toLowerCase() ? 1 : -1;
+        };
+
+        this.sortByEnabled = (itemA, itemB) => {
+            return itemA.enabled && itemB.enabled
+                // If both are enabled sort by name
+                ? this.sortByName(itemA, itemB)
+                // If only one is enabled, we only need to check one
+                : itemA.enabled ? -1 : 1;
+        }
+
+        this.isUpdated = (item) => {
+            return item.installed && item.latestVersion.compare(item.installedVersion) == 1;
+        }
+
+        this.sortByUpdated = (itemA, itemB) => {
+            if (!this.isUpdated(itemA)) return 1;
+            if (!this.isUpdated(itemB)) return -1;
+            return this.sortByName(itemA, itemB);
+        };
+
+        this.sortByInstalled = (itemA, itemB) => {
+            if (!itemA.installed) return 1;
+            if (!itemB.installed) return -1;
+            return this.sortByName(itemA, itemB);
+        };
     }
 
-    render() {
+    render() {        
         let items;
 
-        // Deep copy & reverse order
-        if (this.state.reverseSort) {
-            items = [];
-            let count = window.modsData.length;
+        // Filter out uninstalled mods if needed
+        if (this.props.type === "installed")
+            items = window.modsData.filter((item) => item.installed);
+        else items = window.modsData.map(item => item);
 
-            // Filter out uninstalled mods
-            if (this.props.type === "installed") {
-                window.modsData.forEach((item, index) => {
-                    if (item.installed) items[count - index] = item;
-                });
-                
-                // Remove the empty spaces that might be left
-                items = items.filter(item => item);
-            } else
-                window.modsData.forEach((item, index) => items[count - index] = item);
-        
-        // Only deep copy
-        } else {
-            // Filter out uninstalled mods
-            if (this.props.type === "installed")
-                items = window.modsData.filter((item) => item.installed);
-            else
-                items = window.modsData.map((item) => item);
-        }
+        // Sort if needed
+        let sorter = null;
+        if (this.state.sortBy === "enabled")
+            sorter = this.reverseSorterIfNeeded(this.sortByEnabled);
+        else if (this.state.sortBy === "updated")
+            sorter = this.reverseSorterIfNeeded(this.sortByUpdated);
+        else if (this.state.sortBy === "installed")
+            sorter = this.reverseSorterIfNeeded(this.sortByInstalled);
+
+        if (sorter !== null) items = items.sort(sorter);
 
         // Do searching
         items = items.filter((mod) => {
@@ -103,9 +137,12 @@ export default class ModsList extends React.PureComponent {
 
         // Create a collection item element for each mod
         items = items.map((mod) => {
+            let icon = this.isUpdated(mod) ? "file_download" : "";
+            if (mod.installed && this.props.type !== "installed") icon += "check";
+
             const updateIcon = (
-                mod.installed && mod.latestVersion.compare(mod.installedVersion) == 1
-                    ? <>(<i className="material-icons left">sync</i>)</>
+                icon !== ""
+                    ? <><i className="material-icons left">{icon}</i></>
                     : <></>
             );
 
@@ -150,7 +187,10 @@ export default class ModsList extends React.PureComponent {
             );
         });
 
-        console.log(this.state);
+        const log = (thing) => {
+            console.log(thing);
+            return thing;
+        }
 
         return (
             <div className="collection">
@@ -163,6 +203,37 @@ export default class ModsList extends React.PureComponent {
                             </div>
                             <div className="collapsible-body">
                                 <span>
+                                    Sort by: <br />
+                                    <label>
+                                        <input className="with-gap" name="sortBy" type="radio" value="none"
+                                            checked={this.state.sortBy === "none"}
+                                            onChange={this.handleOptionChange} />
+                                        <span>Name &nbsp; &nbsp; &nbsp; </span>
+                                    </label>
+                                    {
+                                        this.props.type !== "installed" ? (<>
+                                            <label>
+                                                <input className="with-gap" name="sortBy" type="radio" value="installed"
+                                                    checked={this.state.sortBy === "installed"}
+                                                    onChange={this.handleOptionChange} />
+                                                <span>Installed</span>
+                                            </label><br />
+                                        </>) : (<>
+                                            <label>
+                                                <input className="with-gap" name="sortBy" type="radio" value="enabled"
+                                                    checked={this.state.sortBy === "enabled"}
+                                                    onChange={this.handleOptionChange} />
+                                                <span>Enabled</span>
+                                            </label><br />
+                                        </>)
+                                    }
+                                    <label>
+                                        <input className="with-gap" name="sortBy" type="radio" value="updated"
+                                            checked={this.state.sortBy === "updated"}
+                                            onChange={this.handleOptionChange} />
+                                        <span>Updated</span>
+                                    </label>
+                                    <br /><br />
                                     <label>
                                         <input type="checkbox" className="filled-in" state="fuzzySearch" ref={
                                                 this.state.fuzzySearch ? this.makeChecked : this.doNothing
@@ -195,5 +266,12 @@ export default class ModsList extends React.PureComponent {
                 {items}
             </div>
         );
+    }
+
+    componentDidUpdate() {
+        if (this.state.sortBy == "enabled" && this.props.type !== "installed")
+            this.setState({sortBy: "none"});
+        if (this.state.sortBy == "installed" && this.props.type === "installed")
+            this.setState({sortBy: "none"});
     }
 }
