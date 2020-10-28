4185aead9a6661c0632cb8eb1906f947d2c339ef
iSkLz <isklzcode@gmail.com>
2020-10-29 00:42:28 +0100

    Revamped the tooltip component

--- jsx/components/utils/tooltip.jsx
+++ jsx/components/utils/tooltip.jsx
@@ -1,7 +1,27 @@
-export default function Tooltip(props) {
-    return (
-        <span className="tooltipped" data-position={props.position} data-tooltip={props.text}>
-            {props.children};
-        </span>
-    );
+export default class Tooltip extends React.Component {
+    render() {
+        // Merging custom props with provided props into a deep-copied object
+        let elementProps = Object.assign({
+            className: `tooltipped ${this.props.classes || ""}`,
+            "data-position": this.props.position,
+            "data-tooltip": this.props.text,
+            ref: (
+                (elem) => {
+                    this.instance = M.Tooltip.init(elem);
+                }
+            )
+        }, this.props);
+        delete elementProps.children;
+        delete elementProps.element;
+        delete elementProps.classes;
+
+        return (
+            React.createElement(this.props.element || "span", elementProps, this.props.children)
+        );
+    }
+
+    componentWillUnmount() {
+        // Destroy tooltip instance
+        this.instance.destroy();
+    }
 }
