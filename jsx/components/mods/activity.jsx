a85c39cdbc2f3a68e9b960cfc52fdf08ef959906
iSkLz <isklzcode@gmail.com>
2020-10-29 01:58:32 +0100

    Fixed reversed activities length if statement

--- jsx/components/mods/activity.jsx
+++ jsx/components/mods/activity.jsx
@@ -1,11 +1,6 @@
 export default class ActivityView extends React.Component {
     render() {
         if (this.props.activities.length > 0) {
-            return (
-                <h1>No ongoing activites.</h1>
-            );
-        }
-        else {
             const bars = this.props.activities.map((activity) => (
                 <div class="progress">
                     <div class="determinate" style={{width: `${activity.progress}%`}}></div>
@@ -13,6 +8,11 @@ export default class ActivityView extends React.Component {
             ));
 
             return <>{bars}</>;
+        }
+        else {
+            return (
+                <h1>No ongoing activites.</h1>
+            );
         }
     }
 }
