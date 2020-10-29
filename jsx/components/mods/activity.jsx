0739751cbd0329aeed177355715306c826609245
iSkLz <isklzcode@gmail.com>
2020-10-29 01:59:39 +0100

    Fixed class attribute (class => className)

--- jsx/components/mods/activity.jsx
+++ jsx/components/mods/activity.jsx
@@ -2,8 +2,8 @@ export default class ActivityView extends React.Component {
     render() {
         if (this.props.activities.length > 0) {
             const bars = this.props.activities.map((activity) => (
-                <div class="progress">
-                    <div class="determinate" style={{width: `${activity.progress}%`}}></div>
+                <div className="progress">
+                    <div className="determinate" style={{width: `${activity.progress}%`}}></div>
                 </div>
             ));
 
